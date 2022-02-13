0c09314f21da0ecc5efdfcf16a235db5a1b5d0f2
Mohamed Hassan <hafizmohamed114@gmail.com>
2022-02-12 21:51:12 -0500

    Full Site changes

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Typing Animation Script
+var typed = new Typed(".typing", {
+    strings: ["Full Stack Web Developer.", "Mobile App Developer.", "Programmer.", "Designer.", "Freelancer."],
+    typeSpeed:175,
+    backspeed: 150,
+    loop: true
+})
+
+var typed = new Typed(".typing-2", {
+    strings: ["Full Stack Web Developer.", "Mobile App Developer.", "Programmer.", "Designer.", "Freelancer."],
+    typeSpeed:175,
+    backspeed: 150,
+    loop: true
+})
